e060866de9be215b8ce988e179afd9f8c68a42ce
Klauss_Unsen <kolian1000000@mail.ru>
2024-11-29 12:56:44 +0300

    PP_222_v1

---
+++ src/main/java/web/car_back/CarService.java
@@ -0,0 +1,6 @@
+package web.car_back;
+import java.util.List;
+
+public interface CarService {
+    List<Car> getCars(Integer count);
+}
--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,28 +1,29 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.car_back.Car;
+import web.car_back.CarService;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class CarController {
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping("/cars")
     public String showCars(@RequestParam(required = false, defaultValue = "5") Integer count, ModelMap model) {
-        List<Car> cars = Arrays.asList(
-                new Car("Toyota", "Camry", 2020),
-                new Car("Honda", "Civic", 2019),
-                new Car("Ford", "Focus", 2018),
-                new Car("BMW", "3 Series", 2021),
-                new Car("Audi", "A4", 2022)
-        );
-
-        model.addAttribute("cars", count > cars.size() ? cars : cars.subList(0, count));
-
+        List<Car> cars = carService.getCars(count);
+        model.addAttribute("cars", cars);
         return "cars";
     }
 
